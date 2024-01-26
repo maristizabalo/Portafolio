f6183f59c221c07655f592c0b1614321f66efdd0
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2024-01-26 17:53:41 -0500

    Configurando el hero-content
    Se crea componente Hero-content por ahora solo existe un pequeño texto encenerrado en un rectangulo redondeado muy parecido a un boton, aun falta mirar estilos y colores para una buena y bonita visualizaciond e usuarios

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/main/Hero'
+import HeroContent from '@/components/sub/HeroContent'
 
 export default function Home() {
   return (
     <main className='h-screen w-full'>
       <div className='flex flex-col h-full w-full'>
         <Hero />
+        <HeroContent />
       </div>
     </main>
   )
