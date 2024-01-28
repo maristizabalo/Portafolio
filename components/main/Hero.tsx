82d13bf15ebf0184bf3bda00389c515adf263ee9
Maicol Aristizabal <maristizabalo95@gmail.com>
2024-01-28 18:45:12 -0500

    format

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,16 +1,16 @@
 const Hero = () => {
-    return (
-      <div className='relative h-screen w-full overflow-hidden'>
-        <video
-          autoPlay
-          muted
-          loop
-          className='w-full h-full object-cover fixed inset-0 z-[-1]'
-        >
-          <source src='/blackhole2.webm' type='video/webm' />
-        </video>
-      </div>
-    );
-  };
+  return (
+    <div className='relative h-screen w-full overflow-hidden'>
+      <video
+        autoPlay
+        muted
+        loop
+        className='w-full h-full object-cover fixed inset-0 z-[-1]'
+      >
+        <source src='/blackhole2.webm' type='video/webm' />
+      </video>
+    </div>
+  );
+};
 
 export default Hero
